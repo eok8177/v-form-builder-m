4b4bfda2ff04de178b09abf4a55076363fa4d90f
Evgen <eok8177@gmail.com>
2020-02-13 20:06:34 +0200

    All Fields: design

--- src/template/ui/common/controls/AllFieldsControl.vue
+++ src/template/ui/common/controls/AllFieldsControl.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="controlItemWrapper" :class="control.className" :data-control-name="control.name">
         <div class="tools" @click="removeItem(control.name)"><span class="clickable fa fa-times"></span></div>
-        <div class="controlItem row" :id="control.name">
 
-            <label :class="{'bold': control.labelBold, 'italic': control.labelItalic, 'underline': control.labelUnderline}" v-html="control.label"></label>
+        <div :class="control.cssClass" class="controlItem pl-3" :id="control.name">
+            <div class="form-group form-check">
+              <input type="checkbox" class="form-check-input" id="agree">
+              <label class="form-check-label" for="agree" 
+                :class="{'bold': control.labelBold, 'italic': control.labelItalic, 'underline': control.labelUnderline, 'required': control.required}"
+                v-html="control.label"></label>
+            </div>
         </div>
     </div>
 </template>
